9fa4a27c5ddbb6acb4e24263c91de0732d2bc4a8
coni <46821134+coni@users.noreply.github.com>
2022-03-25 19:30:13 +0000

    return classpath for fabric

--- lib/minecraft/classpath.c
+++ lib/minecraft/classpath.c
@@ -21,18 +21,16 @@ int compareLwjglVersion(const char * new, const char * old)
 }
 char *getClasspath_downloadLibraries(cJSON **manifest, const char *path, CURL **session)
 {
-	regex_t regex;
-	regcomp(&regex, "(.*)?org/lwjgl/(.*)?/([0-9.].*)/(.*)?", REG_EXTENDED);
-	int reti;
-
-	int s_classpath = 1;
-	char *classpath = malloc(sizeof(char *) * s_classpath);
+	char *classpath = malloc(sizeof(char *));
 	strcpy(classpath, "");
+
 	const cJSON *lib = NULL;
 	const cJSON *libraries = cJSON_GetObjectItemCaseSensitive(*manifest, "libraries");
-	int s_lwjglClasspath = 1;
-	char *lwjglClasspath = malloc(s_lwjglClasspath*sizeof(char *));
-	char *lwjglVersion = malloc(5 * sizeof(char *));
+
+	char *lwjglClasspath = malloc(sizeof(char *));
+	strcpy(lwjglClasspath, "");
+
+	char *lwjglVersion = malloc(sizeof(char *) * 5);
 	strcpy(lwjglVersion, "0.0.0");
 
 	if (libraries)
@@ -40,61 +38,72 @@ char *getClasspath_downloadLibraries(cJSON **manifest, const char *path, CURL **
 		cJSON_ArrayForEach(lib, libraries)
 		{
 			cJSON *libName = cJSON_GetObjectItemCaseSensitive(lib, "name");
-			// Formatting Classpath pour Fabric, garde ce code
 			char splittedLibName[strlen(libName->valuestring)];
 			strcpy(splittedLibName, libName->valuestring);
 			char *splittedLibNameElt = strtok(splittedLibName, ":");
+
 			char *fullpath = malloc(strlen(path) + 1);
 			char *org = NULL;
-			char *lib = NULL;
+			char *name = NULL;
 			char *version = NULL;
+			int isLwjgl = 0;
 
-			int counter = 0;
-			while (splittedLibNameElt != NULL)
-			{
-				if (counter == 0)
-				{
-					org = malloc(sizeof(char *) * strlen(splittedLibNameElt));
-					strcpy(org, splittedLibNameElt);
-				}
-				else if (counter == 1)
-				{
-					lib = malloc(sizeof(char *) * strlen(splittedLibNameElt));
-					strcpy(lib, splittedLibNameElt);
-				}
-				else if (counter == 2)
-				{
-					version = malloc(sizeof(char *) * strlen(splittedLibNameElt));
-					strcpy(version, splittedLibNameElt);
-				}
+			org = malloc(sizeof(char *) * strlen(splittedLibNameElt));
+			strcpy(org, splittedLibNameElt);
+			splittedLibNameElt =  strtok(NULL,":");
 
-				splittedLibNameElt =  strtok(NULL,":");
-				counter++;
-			}
+			name = malloc(sizeof(char *) * strlen(splittedLibNameElt));
+			strcpy(name, splittedLibNameElt);
+			splittedLibNameElt =  strtok(NULL,":");
 
-			char *splitOrg = strtok(org, ".");
-			s_classpath = strlen(path) + s_classpath + (strlen(lib) + strlen(version)) * 2 + 8;
-			classpath = realloc(classpath, sizeof(char *) * s_classpath);
+			version = malloc(sizeof(char *) * strlen(splittedLibNameElt));
+			strcpy(version, splittedLibNameElt);
+
+			char *splitOrg = strtok(org, ".");	
+			fullpath = realloc(fullpath, sizeof(char *) * (strlen(path) + strlen(org) + (strlen(name) + strlen(version)) * 2) + 8);
+			strcpy(fullpath, path);
+			
 			while (splitOrg)
 			{
-				s_classpath = s_classpath + strlen(splitOrg) + 1;
-				classpath = realloc(classpath, sizeof(char *)*s_classpath);
-				strcat(classpath, splitOrg);
-				strcat(classpath, "/");
+				strcat(fullpath, splitOrg);
+				strcat(fullpath, "/");
+				if (strcmp(splitOrg, "lwjgl") == 0)
+					isLwjgl = 1;
 				splitOrg = strtok(NULL, ".");
 			}
-			strcpy(fullpath, path);
-			strcat(classpath, lib);
-			strcat(classpath, "/");
-			strcat(classpath, version);
-			strcat(classpath, "/");
-			strcat(classpath, lib);
-			strcat(classpath, "-");
-			strcat(classpath,version);
-			strcat(classpath, ".jar");
-			strcat(classpath, ":");
-			printf("%s\n", classpath);
 
+			strcat(fullpath, name);
+			strcat(fullpath, "/");
+			strcat(fullpath, version);
+			strcat(fullpath, "/");
+			strcat(fullpath, name);
+			strcat(fullpath, "-");
+			strcat(fullpath, version);
+			strcat(fullpath, ".jar");
+			
+			if (isLwjgl)
+			{
+				if (strcmp(lwjglVersion, version) != 0)
+				{
+					if (compareLwjglVersion(version, lwjglVersion) == 0)
+					{
+						lwjglClasspath = realloc(lwjglClasspath, sizeof(char *) * (strlen(fullpath) + 1));
+						strcpy(lwjglClasspath, fullpath);
+						strcpy(lwjglVersion, version);
+					}
+					else
+						continue;
+				}
+				lwjglClasspath = realloc(lwjglClasspath, sizeof(char *) * (strlen(lwjglClasspath) + strlen(fullpath) + 1));
+				strcat(lwjglClasspath, ":");
+				strcat(lwjglClasspath, fullpath);
+			}
+			else
+			{
+				classpath = realloc(classpath, sizeof(char *) * (strlen(classpath) + strlen(fullpath) + 1));
+				strcat(classpath, fullpath);
+				strcat(classpath, ":");
+			}
 				// Download Librarie
 			cJSON *libDlInfo = cJSON_GetObjectItemCaseSensitive(lib, "downloads");
 			if (libDlInfo)
@@ -112,8 +121,10 @@ char *getClasspath_downloadLibraries(cJSON **manifest, const char *path, CURL **
 			}
 		}
 	}
-	/* classpath = realloc(classpath, s_classpath+s_lwjglClasspath); */
-	/* strcat(classpath, lwjglClasspath); */
-	/* free(lwjglVersion); */
+	if (strcmp(lwjglClasspath, "") != 0)
+	{
+		classpath = realloc(classpath, strlen(classpath) + strlen(lwjglClasspath));
+		strcat(classpath, lwjglClasspath);
+	}
 	return classpath;
 }
